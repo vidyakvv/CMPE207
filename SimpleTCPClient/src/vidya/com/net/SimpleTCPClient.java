23acbf67fef7d9472c348d40e0c21872f20a5064
vidyakvv <51245746+vidyakvv@users.noreply.github.com>
2019-09-19 15:33:22 -0700

    Update SimpleTCPClient.java
    updated file

--- SimpleTCPClient/src/vidya/com/net/SimpleTCPClient.java
+++ SimpleTCPClient/src/vidya/com/net/SimpleTCPClient.java
@@ -4,10 +4,22 @@ import java.net.*;
 import java.io.*;
 
 public class SimpleTCPClient {
-	public static void main(String[] args) {
-        String hostname = "94.142.241.111";
-        int port = 80;
- 
-        
-    }
+	public static void main(String[] args) throws IOException {
+		String hostname = "94.142.241.111";
+		int port = 23;
+
+		Socket socket = new Socket(hostname, port);
+		System.out.println("Connected to server...");
+
+		InputStream in = socket.getInputStream();
+
+		byte[] bytesRcvd = new byte[2048];
+		int byteCount = in.read(bytesRcvd);
+		while(byteCount != -1) {
+			System.out.println(new String(bytesRcvd));
+			byteCount = in.read(bytesRcvd);
+		}
+		in.close();
+		socket.close(); // Close the socket and its streams
+	}
 }
